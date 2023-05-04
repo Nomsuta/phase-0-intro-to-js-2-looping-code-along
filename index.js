4d7ba9dbcb52c4207a3f402131a31cd7a0cd1d3f
Naomi Odden <munchingpeony@gmail.com>
2023-05-03 22:16:22 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(cardsArray) {
+    const cards = [] 
+    console.log(cards)
+    for (let i = 0; i < cardsArray.length; i++) {
+    cards.push(`Thank you, ${cardsArray[i]}, for the wonderful surprise gift!`)
+    console.log(cards)
+}
+
+return cards;
+}
+
+writeCards(cards);
+
+function countDown(count) {
+while (count > 0) {
+    console.log(count)
+    count -= 1
+}
+    console.log(count)
+}
